11ae767653adcb453081dc235b24293e14ed0b7f
bao3004 <baosum05@gmail.com>
2025-05-09 08:30:56 +0700

    Initial commit

--- deploy.sh
+++ deploy.sh
@@ -1,26 +1,4 @@
-#!/usr/bin/env sh
-
-# abort on errors
-set -e
-
-# build
-npm run build
-
-# navigate into the build output directory
-cd dist
-
-# if you are deploying to a custom domain
-# echo 'www.example.com' > CNAME
-
-git init
-git add -A
-git commit -m 'deploy'
-git branch -M master main
-
-# if you are deploying to https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
-
-# if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:bao3004/my-vue.git main:gh-pages
-
-cd -
+3 steps to deploy:
+>> npm run build
+>> npm run deploy
+>> npm run commit
